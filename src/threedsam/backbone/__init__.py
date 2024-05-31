2fdcb5263e789e4a88457dab0f51c946d0b06688
Morgen Hu PC <2683618775@qq.com>
2024-05-31 14:43:56 +0800

    Chance backbone back to original one
    Backbone now produces 1/8 and 1/2 feature map

--- src/threedsam/backbone/__init__.py
+++ src/threedsam/backbone/__init__.py
@@ -1,7 +1,11 @@
-from .resnet_fpn import ResNetFPN_32
+from .resnet_fpn import ResNetFPN_8_2, ResNetFPN_16_4
+
 
 def build_backbone(config):
     if config['backbone_type'] == 'ResNetFPN':
-        return ResNetFPN_32(config['resnetfpn'])
+        if config['resolution'] == (8, 2):
+            return ResNetFPN_8_2(config['resnetfpn'])
+        elif config['resolution'] == (16, 4):
+            return ResNetFPN_16_4(config['resnetfpn'])
     else:
-        raise ValueError(f"Not supported backbone type.")
+        raise ValueError(f"LOFTR.BACKBONE_TYPE {config['backbone_type']} not supported.")
