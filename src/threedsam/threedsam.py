Fix error about instrinsic matrix scaling

src/threedsam/threedsam.py
@@ -26,7 +26,7 @@ class ThreeDSAM(nn.Module):
         self.fine_matching = FineMatching()
 
         self.iter_num = config['n_iter']
-        self.update_weight = nn.Parameter(torch.tensor([0.8 for _ in range(self.iter_num)]))
+        self.update_weight = nn.Parameter(torch.tensor([0.5 for _ in range(self.iter_num)]))
 
         self.temperature = config['match_coarse']['dsmax_temperature']
 

src/threedsam/threedsam_modules/linear_attention.py
@@ -9,7 +9,7 @@ import torch.nn.functional as F
 from kornia.utils import create_meshgrid
 from einops.einops import rearrange
 
-from ..utils.geometry import get_epipolar_line_std
+from ..utils.geometry import get_epipolar_line_std, get_scaled_K
 
 
 if hasattr(F, 'scaled_dot_product_attention'):
@@ -143,12 +143,15 @@ class One2ManyAttention(Module):
 
     @torch.no_grad()
     def get_candidate_info(self, epipolar_info):
-        scale = epipolar_info['agg_scale']
+        
+        scale_c = epipolar_info['scale']
+        scale_agg = epipolar_info['agg_scale']  
+        scale_K = scale_c * scale_agg
 
-        H0 = epipolar_info['hw0_c'][0] // scale
-        W0 = epipolar_info['hw0_c'][1] // scale
-        H1 = epipolar_info['hw1_c'][0] // scale
-        W1 = epipolar_info['hw1_c'][1] // scale
+        H0 = epipolar_info['hw0_c'][0] // scale_agg
+        W0 = epipolar_info['hw0_c'][1] // scale_agg
+        H1 = epipolar_info['hw1_c'][0] // scale_agg
+        W1 = epipolar_info['hw1_c'][1] // scale_agg
 
         K0 = epipolar_info['K0'].clone()
         K1 = epipolar_info['K1'].clone()
@@ -161,8 +164,8 @@ class One2ManyAttention(Module):
             H1, W1, False, K1.device
         ).flatten(1, 2)  # [1, L, 2] - <x, y>
 
-        K0 = self.get_scaled_K(K0, scale)
-        K1 = self.get_scaled_K(K1, scale)
+        K0 = get_scaled_K(K0, scale_K)
+        K1 = get_scaled_K(K1, scale_K)
 
         R = epipolar_info['R']
         t = epipolar_info['t']
@@ -254,18 +257,6 @@ class One2ManyAttention(Module):
         valid_mask = indices_output != 0  # [N, L, C]
 
         return indices_output, valid_mask
-
-        
-    @torch.no_grad()
-    def get_scaled_K(self, K: torch.Tensor, scale):
-        if K.dim() == 2:
-            K[:2, :] = K[:2, :] / scale
-        elif K.dim() == 3:
-            K[:, :2, :] = K[:, :2, :] / scale
-        else:
-            raise ValueError("Expected tensor of shape: [N, 3, 3] or [3, 3]")
-
-        return K
 
     def update_mask(self, mask, data):
         NotImplemented

src/threedsam/threedsam_modules/structure_extract.py
@@ -4,7 +4,7 @@ from kornia.utils import create_meshgrid
 from einops.einops import rearrange
 
 from ..utils.index_padding import anchor_index_padding
-from ..utils.geometry import estimate_pose
+from ..utils.geometry import estimate_pose, get_scaled_K
 
 INF = 1e9
 
@@ -53,15 +53,18 @@ class StructureExtractor(nn.Module):
         
         epipolar_sample = ~data['non_epipolar'] 
 
+        scale = data['hw0_i'][0] / data['hw0_c'][0]  # 8
         epipolar_info0 = dict(hw0_c = data['hw0_c'],
                              hw1_c = data['hw1_c'], 
                              K0 = data['K0'][epipolar_sample], 
-                             K1 = data['K1'][epipolar_sample])
+                             K1 = data['K1'][epipolar_sample],
+                             scale = scale)
 
         epipolar_info1 = dict(hw0_c = data['hw1_c'],
                              hw1_c = data['hw0_c'],
                              K0 = data['K1'][epipolar_sample], 
-                             K1 = data['K0'][epipolar_sample])
+                             K1 = data['K0'][epipolar_sample],
+                             scale = scale)
 
         depthmap_scale = data['hw0_i'][0] // data['hw0_c'][0]  # 8
         pts_3d0 = data['pts_3d0'][epipolar_sample] # [N', L', 3], L' = 640 * 480
@@ -81,6 +84,9 @@ class StructureExtractor(nn.Module):
         # 2.estimate relative pose using anchor points
         K0 = data['K0'][epipolar_sample].clone()
         K1 = data['K1'][epipolar_sample].clone()
+        K0 = get_scaled_K(K0, scale)
+        K1 = get_scaled_K(K1, scale)
+
         R, t = estimate_pose(pts_2d0, pts_2d1, K0, K1)
 
         epipolar_info0['R'] = R  # [N, 3, 3]